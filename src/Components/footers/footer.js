4d864c10e1c0308ef0567b9c37775d0b30dd18c4
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-02 00:03:41 +0300

    add companyname

--- src/Components/footers/footer.js
+++ src/Components/footers/footer.js
@@ -2,7 +2,7 @@
   <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
     <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
       <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#pablo">
-        blueGray Color
+        S Lucie Apartments
       </a>
       <button className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
         <span className="block relative w-6 h-px rounded-sm bg-white"></span>
@@ -14,17 +14,17 @@
       <ul className="flex flex-col lg:flex-row list-none ml-auto">
           <li className="nav-item">
             <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-              Discover
+              Home 
             </a>
           </li>
           <li className="nav-item">
             <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-              Profile
+              Services
             </a>
           </li>
           <li className="nav-item">
             <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-              Setting
+              Contact
             </a>
           </li>
       </ul>
