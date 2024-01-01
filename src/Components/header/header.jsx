f1333a0d4f93686c84e1369507a0de91570dfcc3
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-01 23:45:03 +0300

    crete header component

---
+++ src/Components/header/header.jsx
@@ -0,0 +1,33 @@
+<nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-blueGray-500 mb-3">
+  <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+      <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#pablo">
+        blueGray Color
+      </a>
+      <button className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
+        <span className="block relative w-6 h-px rounded-sm bg-white"></span>
+        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+      </button>
+    </div>
+    <div className="lg:flex flex-grow items-center">
+      <ul className="flex flex-col lg:flex-row list-none ml-auto">
+          <li className="nav-item">
+            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              Discover
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              Profile
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              Setting
+            </a>
+          </li>
+      </ul>
+    </div>
+  </div>
+</nav>
